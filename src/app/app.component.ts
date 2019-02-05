429764f716b362f74ff528aecaafaa918959d74e
Dan Silcox <dan@MacBook-Pro.local>
2019-02-05 23:17:43 +0000

    Ask Fred\!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Points} from './points';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit  {
     points: Points;
+    fredSays: string;
+    fredSays$ = new BehaviorSubject<string>();
+    private fredsSayings: string[] = [
+        'The thing is',
+        'Basically',
+        'Essentially',
+        'Due to',
+        'Briefly',
+        'Because of',
+        'Here\'s why',
+        'To explain',
+        'The explanation is',
+        'The problem is',
+        'Unfortunately',
+        'However',
+        'In a nutshell',
+        'Owing to',
+        'Considering'
+    ];
+
+    ngOnInit() {
+        this.askFred();
+    }
+
+    askFred() {
+        this.fredSays = this.fredsSayings[this.getRandomInt(0, this.fredsSayings.length)];
+        this.fredSays$.next(this.fredSays);
+    }
 
     calculatePoints() {
         const enumValues = Object.keys(Points)
