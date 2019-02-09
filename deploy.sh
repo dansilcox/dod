d1b180aebe644308f16d56018c2db1f12e7c0149
Dan Silcox <dan@MacBook-Pro.local>
2019-02-09 23:13:16 +0000

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,17 @@
 
 set -e;
 
-BUCKET_NAME="$1";
+# Read the bucket name from the env file below (not committed to git)
+BUCKET_NAME="$(< .env.s3-bucket)"
 DIST_DIR='./dist/dod/';
 
 if [ ! -n "$BUCKET_NAME" ]; then
-    echo "Please provide bucket name as S3 url e.g. s3://foo-bar";
+    echo "Please provide bucket name as S3 url e.g. s3://foo-bar in text file .env.s3-bucket";
     exit 1;
 fi
-
+echo "Preparing to build and deploy app to S3 bucket '$BUCKET_NAME'...";
 echo "Building artifact...";
 ng build;
-echo "Pushing to S3...";
+echo "Pushing to S3 bucket '$BUCKET_NAME'...";
 aws s3 sync "$DIST_DIR" "$BUCKET_NAME";
 echo "Done!";
