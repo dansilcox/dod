f7b7509f8c59e3d56abf1181bff530740d3a5937
Dan Silcox <dan@MacBook-Pro.local>
2019-05-22 22:17:55 +0100

    Ship the change to ship-it shipping script

--- ship-it.sh
+++ ship-it.sh
@@ -10,9 +10,9 @@ if [ ! -n "$BUCKET_NAME" ]; then
     echo "Please provide bucket name as S3 url e.g. s3://foo-bar in text file .env.s3-bucket";
     exit 1;
 fi
-echo "Preparing to build and deploy app to S3 bucket '$BUCKET_NAME'...";
+echo "Preparing to build and deploy production-configured app to S3 bucket '$BUCKET_NAME'...";
 echo "Building artifact...";
-ng build;
+ng build --prod;
 echo "Pushing to S3 bucket '$BUCKET_NAME'...";
 aws s3 sync "$DIST_DIR" "$BUCKET_NAME";
 echo "Done!";
